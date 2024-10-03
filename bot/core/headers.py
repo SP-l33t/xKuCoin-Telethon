1bc1f03bba4602b7078672ad897db87a90479b4c
Slayer <slayerpro27@gmail.com>
2024-10-03 18:45:56 +0300

    Small refactoring with small improvements

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,6 +1,24 @@
 import re
 
 
+headers = {
+    'Accept': 'application/json',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache',
+    'Origin': 'https://www.kucoin.com',
+    'Priority': 'u=1, i',
+    'Referer': 'https://www.kucoin.com/miniapp/tap-game',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Ch-Ua-mobile': '?1',
+    'Sec-Ch-Ua-platform': '"Android"',
+    'X-Requested-With': 'org.telegram.messenger'
+}
+
+
 def get_sec_ch_ua(user_agent):
     pattern = r'(Chrome|Chromium)\/(\d+)\.(\d+)\.(\d+)\.(\d+)'
 
@@ -18,24 +36,3 @@ def get_sec_ch_ua(user_agent):
         return {'Sec-Ch-Ua': sec_ch_ua}
     else:
         return {}
-
-
-headers = {
-    'Accept': 'application/json',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Content-Type': 'application/json',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'no-cache',
-    'Origin': 'https://www.kucoin.com',
-    'Priority': 'u=1, i',
-    'Referer': 'https://www.kucoin.com/miniapp/tap-game',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Ch-Ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-    'Sec-Ch-Ua-mobile': '?1',
-    'Sec-Ch-Ua-platform': '"Android"',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36',
-    'X-Requested-With': 'org.telegram.messenger'
-}
--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -12,7 +12,7 @@ from time import time
 
 from telethon import TelegramClient
 from telethon.errors import *
-from telethon.types import InputBotAppShortName
+from telethon.types import InputBotAppShortName, InputUser
 from telethon.functions import messages
 
 from .agents import generate_random_user_agent
@@ -29,58 +29,62 @@ class Tapper:
     def __init__(self, tg_client: TelegramClient):
         self.tg_client = tg_client
         self.session_name, _ = os.path.splitext(os.path.basename(tg_client.session.filename))
-        self.config = config_utils.get_session_config(self.session_name, CONFIG_PATH)
-        self.proxy = self.config.get('proxy')
-        self.lock = AsyncInterProcessLock(os.path.join(os.path.dirname(CONFIG_PATH), 'lock_files',  f"{self.session_name}.lock"))
-        self.start_param = ''
+        self.lock = AsyncInterProcessLock(
+            os.path.join(os.path.dirname(CONFIG_PATH), 'lock_files',  f"{self.session_name}.lock"))
         self.headers = headers
 
-        self._webview_data = None
+        session_config = config_utils.get_session_config(self.session_name, CONFIG_PATH)
 
+        if not all(key in session_config for key in ('api_id', 'api_hash', 'user_agent')):
+            logger.critical(self.log_message('CHECK accounts_config.json as it might be corrupted'))
+            exit(-1)
+
+        user_agent = session_config.get('user_agent')
+        self.headers['user-agent'] = user_agent
+        self.headers.update(**get_sec_ch_ua(user_agent))
+
+        self.proxy = session_config.get('proxy')
         if self.proxy:
             proxy = Proxy.from_str(self.proxy)
             proxy_dict = proxy_utils.to_telethon_proxy(proxy)
             self.tg_client.set_proxy(proxy_dict)
 
+        self.start_param = ''
+
+        self._webview_data = None
+
     def log_message(self, message) -> str:
         return f"<light-yellow>{self.session_name}</light-yellow> | {message}"
-
-    async def check_user_agent(self):
-        user_agent = self.config.get('user_agent')
-        if not user_agent:
-            user_agent = generate_random_user_agent()
-            self.config['user_agent'] = user_agent
-            await config_utils.update_session_config_in_file(self.session_name, self.config, CONFIG_PATH)
-
-        self.headers['User-Agent'] = user_agent
-        self.headers.update(**get_sec_ch_ua(user_agent))
 
     async def initialize_webview_data(self):
         if not self._webview_data:
             while True:
                 try:
                     peer = await self.tg_client.get_input_entity('xkucoinbot')
-                    input_bot_app = InputBotAppShortName(bot_id=peer, short_name="kucoinminiapp")
+                    bot_id = InputUser(user_id=peer.user_id, access_hash=peer.access_hash)
+                    input_bot_app = InputBotAppShortName(bot_id=bot_id, short_name="kucoinminiapp")
                     self._webview_data = {'peer': peer, 'app': input_bot_app}
                     break
                 except FloodWaitError as fl:
-                    fls = fl.seconds
-
-                    logger.warning(self.log_message(f"FloodWait {fl}. Waiting {fls}s"))
-                    await asyncio.sleep(fls + 3)
-
+                    logger.warning(self.log_message(f"FloodWait {fl}. Waiting {fl.seconds}s"))
+                    await asyncio.sleep(fl.seconds + 3)
                 except (UnauthorizedError, AuthKeyUnregisteredError):
                     raise InvalidSession(f"{self.session_name}: User is unauthorized")
                 except (UserDeactivatedError, UserDeactivatedBanError, PhoneNumberBannedError):
                     raise InvalidSession(f"{self.session_name}: User is banned")
 
     async def get_tg_web_data(self) -> dict[str, str]:
+        if self.proxy and not self.tg_client._proxy:
+            logger.critical(self.log_message('Proxy found, but not passed to TelegramClient'))
+            exit(-1)
+
         init_data = {}
         async with self.lock:
             try:
                 if not self.tg_client.is_connected():
                     await self.tg_client.connect()
                 await self.initialize_webview_data()
+                await asyncio.sleep(random.uniform(1, 2))
 
                 ref_id = settings.REF_ID if random.randint(0, 100) <= 85 \
                     else "cm91dGU9JTJGdGFwLWdhbWUlM0ZpbnZpdGVyVXNlcklkJTNENTI1MjU2NTI2JTI2cmNvZGUlM0Q="
@@ -231,9 +235,8 @@ class Tapper:
             return None
 
     async def run(self) -> None:
-        await self.check_user_agent()
-        random_delay = random.randint(1, settings.START_DELAY)
-        logger.info(self.log_message(f"Bot will start in <ly>{random_delay}s</ly>"))
+        random_delay = random.uniform(1, settings.START_DELAY)
+        logger.info(self.log_message(f"Bot will start in <ly>{int(random_delay)}s</ly>"))
         await asyncio.sleep(random_delay)
 
         access_token_created_time = 0
