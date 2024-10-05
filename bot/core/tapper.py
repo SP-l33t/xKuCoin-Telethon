b9b460825fed59d878f61234163863872651380a
Slayer <slayerpro27@gmail.com>
2024-10-05 13:39:34 +0300

    Yet another attempt to avoid the Deadlock

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -15,7 +15,6 @@ from telethon.errors import *
 from telethon.types import InputBotAppShortName, InputUser
 from telethon.functions import messages
 
-from .agents import generate_random_user_agent
 from bot.config import settings
 from bot.utils import logger, log_error, proxy_utils, config_utils, AsyncInterProcessLock, CONFIG_PATH
 from bot.exceptions import InvalidSession
@@ -54,7 +53,7 @@ class Tapper:
         self._webview_data = None
 
     def log_message(self, message) -> str:
-        return f"<light-yellow>{self.session_name}</light-yellow> | {message}"
+        return f"<ly>{self.session_name}</ly> | {message}"
 
     async def initialize_webview_data(self):
         if not self._webview_data:
